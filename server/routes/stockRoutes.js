5542e502448985e9027ae8e52c633ae9a60929e4
Enbani <enbani.moore@gmail.com>
2019-07-05 12:53:44 -0400

    adding route to retrieve all saved stocks and to remove a stock

--- server/routes/stockRoutes.js
+++ server/routes/stockRoutes.js
@@ -10,7 +10,17 @@ const apiKey = process.env.WTD_API_KEY;
 const router = express.Router();
 
 // fetch overview stock data
-router.get('/overview', async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const allStocks = await Stock.find({});
+    return res.send({ payload: allStocks });
+  } catch (e) {
+    return res.status(400).send({ error: e.message });
+  }
+});
+
+
+router.get('/add-stock', async (req, res) => {
   const { stock } = req.query;
 
   try {
@@ -40,6 +50,17 @@ router.get('/historical', async (req, res) => {
     const historicalData = await axios.get(`https://api.worldtradingdata.com/api/v1/history?symbol=${stock}&api_token=${apiKey}&sort=newest`);
     console.log(historicalData.data);
     return res.send({ payload: historicalData.data });
+  } catch (e) {
+    return res.status(400).send({ error: e.message });
+  }
+});
+
+router.delete('/remove-stock/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const stock = await Stock.findOneAndRemove({ _id: id });
+    return res.send({ payload: 'stock removed.' });
   } catch (e) {
     return res.status(400).send({ error: e.message });
   }
